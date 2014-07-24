ec3f8c5d5b12dbbceb2ee006009aebbb7d2ffb94
Ian Smith <iansmith@acm.org>
2014-07-24 16:05:25 -0700

    updated to make the dockerfile set LD_LIBRARY_PATH

--- sample1/go/src/sample1/wuclient/wuclient.go
+++ sample1/go/src/sample1/wuclient/wuclient.go
@@ -1,6 +1,6 @@
 //
 //  Weather update client.
-//  Connects SUB socket to tcp://localhost:5556
+//  Connects SUB socket to tcp://weather-server:5556
 //  Collects weather updates and finds avg temp in zipcode
 //
 
